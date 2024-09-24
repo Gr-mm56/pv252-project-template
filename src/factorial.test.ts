60ac7f787d6eb41939f30b39c49198894519c686
Gr-mm56 <xspace39@stud.fit.vutbr.cz>
2024-09-24 16:11:14 +0200

    fix unit tests

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -4,7 +4,7 @@ test("factorial-5", () => {
   expect(factorial(5)).toBe(120);
 });
 test("factorial-0", () => {
-  expect(factorial(0)).toBe(0);
+  expect(factorial(0)).toBe(1);
 });
 
 test("factorial-minus", () => {

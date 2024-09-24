00e4f4fed1d042a62850660e5fea4bdfff110cf1
Gr-mm56 <xspace39@stud.fit.vutbr.cz>
2024-09-24 17:15:21 +0200

    100% unit test coverage

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -1,4 +1,4 @@
-import { factorial } from "./factorial.ts";
+import { factorial, initFactorialUi } from "./factorial.ts";
 
 test("factorial-5", () => {
   expect(factorial(5)).toBe(120);
@@ -6,10 +6,19 @@ test("factorial-5", () => {
 test("factorial-0", () => {
   expect(factorial(0)).toBe(1);
 });
-
+test("factorial-16", () => {
+  expect(factorial(16)).toBe(20922789888000 );
+});
 test("factorial-minus", () => {
   const will_throw = () => {
     factorial(-1);
   };
   expect(will_throw).toThrow("Negative numbers not supported");
 });
+test("set factorial component innerHTML", () =>{
+  const mockComponent = { innerHTML: "" } as HTMLElement;
+  initFactorialUi(mockComponent);
+  expect(mockComponent.innerHTML).toBe(
+    'Factorial value <code>5!</code> is <code>120</code>.'
+  );
+})
--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -1,5 +1,18 @@
-import { fibonacci } from "./fibonacci.ts";
+import { fibonacci, initFibonacciUi } from "./fibonacci.ts";
 
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+test("negative-fibonacci", () => {
+  const will_throw = () => {
+    fibonacci(-1);
+  };
+  expect(will_throw).toThrow("Cannot compute on negative numbers");
+});
+test("set fibonacci component innerHTML", () =>{
+  const mockComponent = { innerHTML: "" } as HTMLElement;
+  initFibonacciUi(mockComponent);
+  expect(mockComponent.innerHTML).toBe(
+    '5th fibonacci number is <code>5</code>'
+  );
+})

353180111a895c924b67bb1fd4d31451bf34fccd
Gr-mm56 <xspace39@stud.fit.vutbr.cz>
2024-09-24 16:08:13 +0200

    break unit tests

--- src/factorial.test.ts
+++ src/factorial.test.ts
@@ -3,6 +3,9 @@ import { factorial } from "./factorial.ts";
 test("factorial-5", () => {
   expect(factorial(5)).toBe(120);
 });
+test("factorial-0", () => {
+  expect(factorial(0)).toBe(0);
+});
 
 test("factorial-minus", () => {
   const will_throw = () => {

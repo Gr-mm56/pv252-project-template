9ca9ad5e4378c5f922330c9f46e61f4647bf230f
Gr-mm56 <xspace39@stud.fit.vutbr.cz>
2024-09-25 18:57:00 +0200

    change to a different site

--- test-ui/new_website.spec.ts
+++ test-ui/new_website.spec.ts
@@ -1,53 +1,45 @@
 import { expect } from "@playwright/test";
 import { test } from "./coverage_wrapper";
 
-//page performance is abysmal, some tests fail due to timeout
-
 test.beforeEach(async ({ page }) => {
-  await page.goto("https://www.metal-archives.com");
-  await page.waitForURL("https://www.metal-archives.com/");
+  await page.goto("https://www.is.muni.cz/?lang=cs");
 });
-test("find existing band", async ({ page }) => {
-  await page.fill("#searchQueryBox", "Keldian");
-  await page.click(".btn_submit");
-  await page.waitForURL("https://www.metal-archives.com/bands/Keldian/89938");
-  expect(page.url()).toContain("https://www.metal-archives.com/bands/Keldian");
+test("verify czech and english version", async ({ page }) => {
+  await expect(
+    page.locator("[aria-label='Informační systém Masarykovy univerzity']"),
+  ).toBeVisible();
+  await page.click("[href='./?lang=en']");
+  await expect(
+    page.locator("[aria-label='Masaryk University Information System']"),
+  ).toBeVisible();
 });
-test("submit empty search", async ({ page }) => {
-  await page.click(".btn_submit");
-  await page.waitForURL("https://www.metal-archives.com/search**");
-  expect(page.url()).toContain("https://www.metal-archives.com/search");
-  await page.waitForSelector("text=Please enter at least one keyword");
-  await expect(page.locator("#content_wrapper")).toContainText(
-    "Please enter at least one keyword",
+test("login with invalid credentials", async ({ page }) => {
+  await page.click("[aria-label='Přihlášení do IS MU']");
+  await page.waitForURL("https://muni.islogin.cz/login/*");
+  await page.fill("[name='credential_0']", "invalid");
+  await page.fill("[name='credential_1']", "invalid");
+  await page.click("[type='submit']");
+  await expect(page.locator(".chyba")).toBeInViewport();
+});
+test("find a course", async ({ page }) => {
+  await page.click("[aria-label='Vyhledávání']");
+  await expect(page.locator("#is_search")).toBeVisible();
+  await page.fill("[name='search']", "PV252");
+  await page.click("[value='Vyhledat']");
+  await page
+    .locator("text=Frontend Web Development and User Experience")
+    .first()
+    .click();
+  expect(page.url()).toContain(
+    "https://is.muni.cz/predmet/fi/podzim2024/PV252",
   );
 });
-test("find non-existing band", async ({ page }) => {
-  await page.fill("#searchQueryBox", "564658");
-  await page.click(".btn_submit");
-  await page.waitForURL("https://www.metal-archives.com/search**");
-  expect(page.url()).toContain("https://www.metal-archives.com/search");
-  await page.waitForSelector("text=No matches found.");
-  await expect(page.locator("#content_wrapper")).toContainText(
-    "No matches found.",
-  );
-});
-test("registration", async ({ page }) => {
-  await page.click("[href='https://www.metal-archives.com/user/signup']");
-  await page.waitForURL("https://www.metal-archives.com/user/signup");
-  await expect(page.locator("#username")).toBeVisible();
-  await expect(page.locator("#password1")).toBeVisible();
-  await expect(page.locator("#password2")).toBeInViewport();
-  await expect(page.locator("#email")).toBeInViewport();
-});
-test("use advanced search ", async ({ page }) => {
-  await page.click("text=Advanced search");
-  await page.waitForURL("https://www.metal-archives.com/search/advanced/*");
-  await page.click("[href='#albums']");
-  await expect(page.locator(".searchFilterTitle").nth(1)).toContainText(
-    "Search for releases",
-  );
-  await page.fill("#releaseTitle", "With Doom We Come");
-  await page.click("[value='Perform search']:visible");
-  await expect(page.locator("#searchResultsAlbum")).toContainText("Summoning");
+test("find a book", async ({ page }) => {
+  await page.goto("https://is.muni.cz/?lang=en");
+  await page.locator("text=Textbooks").click();
+  await page
+    .getByPlaceholder("Zadejte hledaný výraz, název, nebo autora")
+    .fill("Úvod do informatiky");
+  await page.click("[value=Vyhledat]");
+  await expect(page.locator(".autori")).toContainText("Hliněný, Petr");
 });
